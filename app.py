3a8ea90ea1ed89de8b8f7befb52a086770b5b17d
常易 <aa531811820@gmail.com>
2023-04-10 02:26:58 +0800

    [unity] get unity json

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import requests
 import os
 import json
+
 USER_INFO_PATH = os.path.join(os.path.dirname(__file__), "user_info.json")
 
 app = Flask(__name__)
@@ -26,24 +27,36 @@ def on_dragging():
     else:
         return jsonify({"status": "error"})
 
+
 def get_url(url):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
+        print(data)
         response = jsonify(data)
-        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add("Access-Control-Allow-Origin", "*")
         return response
     else:
         return "下载JSON文件失败"
+
+
 @app.route("/chart/flare.json")
 def get_flare():
     url = "https://echarts.apache.org/examples/data/asset/data/flare.json"
     return get_url(url)
 
+
 @app.route("/chart/les.json")
 def get_les():
     url = "https://echarts.apache.org/examples/data/asset/data/les-miserables.json"
     return get_url(url)
+
+
+@app.route("/chart/unity.json")
+def get_les():
+    url = "https://echarts.apache.org/examples/data/asset/data/les-miserables.json"
+    return get_url(url)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
