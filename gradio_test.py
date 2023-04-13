92c76c7c46d0ce98b37b1ed4441eaffd4d421893
lichi.jesse <lichi.jesse@bytedance.com>
2023-04-13 16:22:32 +0800

    add draggable plot

--- gradio_test.py
+++ gradio_test.py
@@ -5,7 +5,7 @@ def text_analysis(text):
     html = """
 <head>
   <title>A</title>
-  <script src="https://cdn.jsdelivr.net/npm/echarts@5.4.1/dist/echarts.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js"></script>
 </head>
 
 <body>
@@ -13,26 +13,147 @@ def text_analysis(text):
   <script>
     var myChart = echarts.init(document.getElementById("main"));
 
-    var option = {
+    var option;
+    const symbolSize = 20;
+    const data = [
+      [40, -10],
+      [-30, -5],
+      [-76.5, 20],
+      [-63.5, 40],
+      [-22.1, 50]
+    ];
+    option = {
       title: {
-        text: "ECharts"
+        text: "Try Dragging these Points",
+        left: "center"
       },
-      tooltip: {},
-      legend: {
-        data: ["销量"]
+      tooltip: {
+        triggerOn: "none",
+        formatter: function (params) {
+          return (
+            "X: " +
+            params.data[0].toFixed(2) +
+            "<br>Y: " +
+            params.data[1].toFixed(2)
+          );
+        }
+      },
+      grid: {
+        top: "8%",
+        bottom: "12%"
       },
       xAxis: {
-        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        min: -100,
+        max: 70,
+        type: "value",
+        axisLine: { onZero: false }
       },
-      yAxis: {},
-      series: [{
-        name: "销量",
-        type: "bar",
-        data: [5, 20, 36, 10, 10, 20]
-      }]
+      yAxis: {
+        min: -30,
+        max: 60,
+        type: "value",
+        axisLine: { onZero: false }
+      },
+      dataZoom: [
+        {
+          type: "slider",
+          xAxisIndex: 0,
+          filterMode: "none"
+        },
+        {
+          type: "slider",
+          yAxisIndex: 0,
+          filterMode: "none"
+        },
+        {
+          type: "inside",
+          xAxisIndex: 0,
+          filterMode: "none"
+        },
+        {
+          type: "inside",
+          yAxisIndex: 0,
+          filterMode: "none"
+        }
+      ],
+      series: [
+        {
+          id: "a",
+          type: "line",
+          smooth: true,
+          symbolSize: symbolSize,
+          data: data
+        }
+      ]
     };
+    setTimeout(function () {
+      // Add shadow circles (which is not visible) to enable drag.
+      myChart.setOption({
+        graphic: data.map(function (item, dataIndex) {
+          return {
+            type: "circle",
+            position: myChart.convertToPixel("grid", item),
+            shape: {
+              cx: 0,
+              cy: 0,
+              r: symbolSize / 2
+            },
+            invisible: true,
+            draggable: true,
+            ondrag: function (dx, dy) {
+              onPointDragging(dataIndex, [this.x, this.y]);
+            },
+            onmousemove: function () {
+              showTooltip(dataIndex);
+            },
+            onmouseout: function () {
+              hideTooltip(dataIndex);
+            },
+            z: 100
+          };
+        })
+      });
+    }, 0);
+    window.addEventListener("resize", updatePosition);
+    myChart.on("dataZoom", updatePosition);
+    function updatePosition() {
+      myChart.setOption({
+        graphic: data.map(function (item, dataIndex) {
+          return {
+            position: myChart.convertToPixel("grid", item)
+          };
+        })
+      });
+    }
+    function showTooltip(dataIndex) {
+      myChart.dispatchAction({
+        type: "showTip",
+        seriesIndex: 0,
+        dataIndex: dataIndex
+      });
+    }
+    function hideTooltip(dataIndex) {
+      myChart.dispatchAction({
+        type: "hideTip"
+      });
+    }
+    function onPointDragging(dataIndex, pos) {
+      data[dataIndex] = myChart.convertFromPixel("grid", pos);
+      // Update data
+      myChart.setOption({
+        series: [
+          {
+            id: "a",
+            data: data
+          }
+        ]
+      });
+    }
 
     myChart.setOption(option);
+
+
+
   </script>
 </body>
 """
